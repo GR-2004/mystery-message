073345485734499a9a08b20279ae9763f639dc77
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-05-31 18:04:38 +0530

    add verificationEmail and sign-up route functionality

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,22 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse>{
+    try {
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: 'Mystry message | Veriication code',
+            react: VerificationEmail({username, otp:verifyCode}),
+          });
+        return {success: true, message: "Verification code send successfully"}
+    } catch (error) {
+        console.error("Error while sending verification email", error);
+        return {success: false, message: "Failed to send verification code"}
+    }
+}
